6d31e78b4366668d6c74ff55952d58792e46c5d6
Pavel Leskavets <leskovecpavel7@gmail.com>
2019-03-08 21:27:06 +0300

    my implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    let arrayOfPrimeNumbers = [];
+    for(let i = 2; i <= base; i++) {
+      if(base % i == 0) { 
+        arrayOfPrimeNumbers.push(i);
+        base = base / i;
+        i--;    
+      }
+      else if(i == base) {
+        arrayOfPrimeNumbers.push(i)
+      }    
+      else {
+        continue
+      }
+    }
+    let countOfZero = 0;
+    for(let j = 0; j < arrayOfPrimeNumbers.length; j++) {
+    let num =  arrayOfPrimeNumbers[j];
+    let arrOFBigNumbers = arrayOfPrimeNumbers.filter(function(number) {
+      return number == arrayOfPrimeNumbers[j];
+    });  
+    let count = 0;
+    for( ;number / num > 1;) {
+      count = count +  Math.floor(number / num);
+      num = num * arrayOfPrimeNumbers[j]; 
+  }
+    if(countOfZero == 0) {
+    countOfZero = Math.floor(count / arrOFBigNumbers.length);
+    arrOFBigNumbers.length = 0;
+    }
+    else if(countOfZero > Math.floor(count / arrOFBigNumbers.length)) {
+      countOfZero = Math.floor(count / arrOFBigNumbers.length);
+      arrOFBigNumbers.length = 0;
+    } 
+  }
+return countOfZero
 }
